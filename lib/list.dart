bd1fd1589b884ab91219e50f7d6f2735aa057464
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-01-28 16:08:48 -0300

    update commit

--- lib/list.dart
+++ lib/list.dart
@@ -5,13 +5,15 @@ void main() {
   final nomeCompleto = (nome ?? "Ricardo") + sobrenome;
   print(nomeCompleto.toUpperCase());
 
-  const jogos = ["eFootball", "Rocket League", "Dark Souls"];
+  const jogos = <String>["eFootball", "Rocket League", "Dark Souls"];
   print(jogos);
 
   List<String> consoles = ["Playstation", "Xbox", "Super Nintendo", "PC"];
   print(consoles);
 
   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  numbers.add(50);
+  print(numbers);
 
   numbers.map((item) => {print(item > 5)});
 }
