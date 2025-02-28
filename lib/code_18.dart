5edee09e7594dc72a5eb3a9553d9197967af1d61
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-02-28 15:39:21 -0300

    update commit

--- lib/code_18.dart
+++ lib/code_18.dart
@@ -1,7 +1,25 @@
 void main() {
-  final nome = "Ricardo";
-  print(nome);
+  final curso = Cursos(data: 2025, nome: "B7Web", valor: 99.99);
+  print("O nome do curso que vou cursar em ${curso.data} é: ${curso.nome}.");
 
-  final conta = 1 + 1;
-  print(conta);
+  final nomes = ["Ricardo", "Moniky", "Letícia"];
+
+  final pessoas = nomes.map((nome) => Pessoa(nome)).toList();
+  for (var pessoa in pessoas) {
+    print("Olá ${pessoa.nome}");
+  }
+}
+
+class Cursos {
+  final int? data;
+  final String? nome;
+  final double? valor;
+
+  const Cursos({required this.data, this.nome, this.valor});
+}
+
+class Pessoa {
+  String nome;
+
+  Pessoa(this.nome);
 }
